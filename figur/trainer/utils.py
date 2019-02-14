dc4ac9fb4380298b6e5b77ebdfcb5c1a617dd764
severinsimmler <severin.simmler@posteo.de>
2019-02-14 13:32:54 +0100

    fix: gpu

--- figur/trainer/utils.py
+++ figur/trainer/utils.py
@@ -13,7 +13,7 @@ def collect_features(embeddings, gpu):
     flair.device = torch.device("cpu")
     print(flair.device)
     mapping = {"fasttext": WordEmbeddings("de"),
-               "bert": BertEmbeddings("bert-base-multilingual-cased"))}#,
+               "bert": BertEmbeddings("bert-base-multilingual-cased")}#,
                #"flair-forward": FlairEmbeddings("german-forward"),
                #"flair-backward": FlairEmbeddings("german-backward")}
     for embedding in embeddings:
